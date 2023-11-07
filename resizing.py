7142612ada369416d7ca60e96578cbde0d31e632
ksarin10 <128098055+ksarin10@users.noreply.github.com>
2023-11-07 16:27:59 -0500

    Update resizing.py

--- resizing.py
+++ resizing.py
@@ -267,56 +267,55 @@ svs_dir = "/Users/krishsarin/Downloads/Krish/resized_masks"
 image_output_dir = "/Users/krishsarin/Downloads/Krish/Training/img2"
 mask_output_dir = "/Users/krishsarin/Downloads/Krish/Training/mask2"
 
-# Patch size
 patch_size = 512
 
-# Loop through SVS files in the directory
+
 for filename in os.listdir(svs_dir):
     if filename.endswith(".svs"):
-        # Get the SVS file path and corresponding mask path
+       
         svs_path = os.path.join(svs_dir, filename)
         mask_path = os.path.join("/Users/krishsarin/Downloads/Krish/resized_masks", filename.replace(".svs", "_mask.png"))
 
-        # Open the SVS file
+    
         slide = OpenSlide(svs_path)
         dims = slide.level_dimensions
 
-        # Get the dimensions of the level one image
+        
         level_two_dim = dims[2]
-        image_width, image_height = level_two_dim  # Assign image dimensions
+        image_width, image_height = level_two_dim 
 
-        # Resize the SVS image to match the level one dimensions
+      
         svs_image = slide.read_region((0, 0), 2, level_two_dim).convert("RGB")
 
-        # Open the mask and resize it to match the image dimensions
+      
         Image.MAX_IMAGE_PIXELS = None
         mask = Image.open(mask_path)
         mask = mask.resize(level_two_dim)
 
-        image_filename = os.path.splitext(os.path.basename(svs_path))[0]  # Get the filename without extension
+        image_filename = os.path.splitext(os.path.basename(svs_path))[0]  
         image_subdir = os.path.join(image_output_dir, image_filename)
         mask_subdir = os.path.join(mask_output_dir, image_filename)
 
         os.makedirs(image_subdir, exist_ok=True)
         os.makedirs(mask_subdir, exist_ok=True)
 
-        index = 0  # Initialize the index to 0
+        index = 0  
 
-        # Loop through the image to create patches
+    
         for y in range(0, image_height, patch_size):
             for x in range(0, image_width, patch_size):
-                # Define the coordinates for the current patch
+              
                 patch_coords = (x, y, x + patch_size, y + patch_size)
         
-                # Crop the patch from the image and mask
+            
                 image_patch = svs_image.crop(patch_coords)
                 mask_patch = mask.crop(patch_coords)
         
-                # Save the patches with sequential numbering
+            
                 image_patch.save(os.path.join(image_subdir, f"image_patch_{index}.png"), format="PNG")
                 mask_patch.save(os.path.join(mask_subdir, f"mask_patch_{index}.png"), format="PNG")
         
-                index += 1  # Increment the index for the next patch
+                index += 1  
                 
 
 
@@ -324,54 +323,49 @@ for filename in os.listdir(svs_dir):
 train_image_dir = "/Users/krishsarin/Downloads/Krish/level1/output_img/120375"
 train_mask_dir = "/Users/krishsarin/Downloads/Krish/level1/output_mask/120375"
 
-# Output base directory
 output_base_dir = "/Users/krishsarin/Downloads/Krish/Training/level1"
 
-# Output directories for images and masks
 output_image_dir = os.path.join(output_base_dir, "img2c")
 output_mask_dir = os.path.join(output_base_dir, "mask2c")
 
-# Create output directories for images and masks if they don't exist
 os.makedirs(output_image_dir, exist_ok=True)
 os.makedirs(output_mask_dir, exist_ok=True)
 
-# Copy the patch directory (e.g., "120378") into the img2c and mask2c directories
+
 output_image_patch_dir = os.path.join(output_image_dir, os.path.basename(train_image_dir))
 output_mask_patch_dir = os.path.join(output_mask_dir, os.path.basename(train_mask_dir))
 
-# Create output directories for the patch directories
+
 os.makedirs(output_image_patch_dir, exist_ok=True)
 os.makedirs(output_mask_patch_dir, exist_ok=True)
 
-# List image files in the image directory
+
 image_files = [f for f in os.listdir(train_image_dir) if f.endswith(".png")]
 
 # Traverse the list of image files
 for image_file in image_files:
     image_path = os.path.join(train_image_dir, image_file)
     
-    # Extract the patch number from the image file name
+    
     image_patch_number = int(image_file.split('_')[-1].split('.')[0])
     
-    # Create the corresponding mask file name
+    
     mask_file = f"mask_patch_{image_patch_number}.png"
     mask_path = os.path.join(train_mask_dir, mask_file)
 
-    # Check if the mask file exists
+  
     if os.path.exists(mask_path):
-        # Load the mask
+       
         mask = Image.open(mask_path)
 
         # Analyze the mask 
         mask_array = np.array(mask)
         has_cancer = np.any(mask_array > 0)
 
-        # If the mask has cancer annotations, copy the image and mask to the output directory
         if has_cancer:
             print(f"Processing image: {image_path}")
             print(f"Matching mask: {mask_path}")
             
-            # Copy the image and mask to the patch directories
             image_output_path = os.path.join(output_image_patch_dir, image_file)
             mask_output_path = os.path.join(output_mask_patch_dir, mask_file)
             print(f"Copying image to: {image_output_path}")
