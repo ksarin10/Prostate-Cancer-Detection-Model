68bc85c2e31895f29242b894097d8e0234c4d65e
ksarin10 <128098055+ksarin10@users.noreply.github.com>
2024-01-03 21:20:29 -0500

    Update normalization.py

--- normalization.py
+++ normalization.py
@@ -9,10 +9,12 @@ import numpy as np
 import cv2
 from matplotlib import pyplot as plt
 
-def stain_normalization(input_image_path):
-    # Load the image
-    img = cv2.imread(input_image_path, 1)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+def stain_norm(input_image):
+    if isinstance(input_image, str):  # Check if the input is a file path
+        input_image = cv2.imread(input_image)  # Load the image
+
+    # Convert the input image to a format expected by the function
+    img = cv2.cvtColor(input_image, cv2.COLOR_BGR2RGB)
 
     Io = 240
     alpha = 1
@@ -27,7 +29,13 @@ def stain_normalization(input_image_path):
     OD = -np.log10((img.astype(float) + 1) / Io)
 
     ODhat = OD[~np.any(OD < beta, axis=1)]
-    eigvals, eigvecs = np.linalg.eigh(np.cov(ODhat.T))
+
+    # Regularize the covariance matrix
+    epsilon = 1e-5
+    cov_matrix = np.cov(ODhat.T)
+    cov_matrix += np.eye(cov_matrix.shape[0]) * epsilon  # Add a small constant epsilon to the diagonal
+
+    eigvals, eigvecs = np.linalg.eigh(cov_matrix)
 
     That = ODhat.dot(eigvecs[:, 1:3])
     phi = np.arctan2(That[:, 1], That[:, 0])
@@ -54,4 +62,8 @@ def stain_normalization(input_image_path):
     Inorm = np.reshape(Inorm.T, (h, w, 3)).astype(np.uint8)
 
     return Inorm
+
+
+
+
 
